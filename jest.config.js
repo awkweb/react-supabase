e7eee32e30cc5f8f45140d178e4751b25b66f104
Tom Meagher <tom@meagher.co>
2021-04-26 09:45:56 -0400

    refactor(tsconfig): setup

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     testRegex: '/test/.*\\.test\\.tsx$',
-    globals: {
-        'ts-jest': {
-            tsconfig: 'tsconfig.test.json',
-        },
-    },
     watchPlugins: [
         'jest-watch-typeahead/filename',
         'jest-watch-typeahead/testname',
